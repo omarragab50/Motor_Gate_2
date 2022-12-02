e17dc1ff513ecdbdd98e817fcd8abfff71edab30
omar <omerragb55@gmail.com>
2022-12-03 01:41:47 +0200

    modify a signup lognin and scrool botun

--- main.js
+++ main.js
@@ -2,3 +2,22 @@ let subMenu = document.getElementById("subMenu");
 function toggleMenu () {
     subMenu.classList.toggle("open-menu");
 }
+
+
+let span = document.querySelector(".up");
+
+window.onscroll = function () {
+    if (this.scrollY >= 800) {
+        span.classList.add("show");
+    }else {
+        span.classList.remove("show");
+    }
+}
+
+
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
